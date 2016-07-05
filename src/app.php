7a8c911a8d019ecef743dc3b499967f10adaa60e
Clark Zhang <czhang@altima-agency.com>
2016-07-05 15:48:18 +0800

    add symfony\routing component

---
+++ src/app.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
+$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('testRoute', new Routing\Route('/testroute'));
+
+return $routes;
